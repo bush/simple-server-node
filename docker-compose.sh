742ccb5253e4612112095585c8652fa493fc6544
Dave Bush <davebush@gmail.com>
2016-08-24 14:08:27 -0400

    Changed host image to be created with closest relative of the resin image
    Signed-off-by: Dave Bush <davebush@gmail.com>

--- docker-compose.sh
+++ docker-compose.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 npm install --production --unsafe-perm && npm cache clean
-sed -e 's/resin\/%%RESIN_MACHINE_NAME%%-//' Dockerfile.template > Dockerfile.tmp
+sed -e 's/resin\/%%RESIN_MACHINE_NAME%%-node:slim/node:6.3.1-slim/' Dockerfile.template > Dockerfile.tmp
 npm install
 #docker build -t simple-server-node -f Dockerfile.tmp .
 docker-compose up -d
